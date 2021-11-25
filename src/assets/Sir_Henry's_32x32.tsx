5a32414e3d02fe8400e8be77b81bbc1e0462968e
lena-hinrichsen <hinrichsen@hab.de>
2021-11-25 13:28:26 +0100

    delete tsx

--- src/assets/Sir_Henry's_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="Sir_Henry's_32x32" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="Sir_Henry's_32x32.png" width="512" height="512"/>
-</tileset>
